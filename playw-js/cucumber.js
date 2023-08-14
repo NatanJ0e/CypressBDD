c0f676b5a72eaf13451558d40bb63bdb8f5a4dd0
NatanJ0e <natanael_ferreira2@hotmail.com>
2023-08-14 08:47:45 -0300

    changes gitignore

--- playw-js/cucumber.js
+++
@@ -1,9 +0,0 @@
-const common = ` 
-  --require setup/assertions.js 
-  --require config/hooks.js 
-  --require step-definitions/**/*.steps.js
-  `;
-
-module.exports = {
-  default: `${common} features/**/*.feature`
-};
